c9b2d1cd2510350f69a301ebe319464db9c9468f
vuong tran <ext.vuong.tran@gxbank.net>
2024-10-20 16:21:50 +0700

    init

---
+++ catalog-service/src/main/java/com/example/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
