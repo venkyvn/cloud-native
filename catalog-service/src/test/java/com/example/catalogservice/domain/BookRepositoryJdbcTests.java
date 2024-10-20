6ce72bca4bade059a64f912d7c6f5cc6c9726ffe
vuong tran <ext.vuong.tran@gxbank.net>
2024-10-20 17:22:18 +0700

    integration with test container

---
+++ catalog-service/src/test/java/com/example/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -0,0 +1,38 @@
+package com.example.catalogservice.domain;
+
+import com.example.catalogservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJdbcTest
+@Import(DataConfig.class) // need to enable auditing
+@AutoConfigureTestDatabase( // using test container instead of default behavior
+        replace = AutoConfigureTestDatabase.Replace.NONE
+)
+@ActiveProfiles("integration")
+public class BookRepositoryJdbcTests {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate; // low-level interacting with db
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        var bookIsbn = "123467890";
+        var book = Book.of(bookIsbn, "title", "author", 12.0);
+        jdbcAggregateTemplate.insert(book);
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(bookIsbn);
+    }
+}
