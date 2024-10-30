c2fbfe54cd41fcdf0f231ed3515cde28ece6a4ce
vuong tran <ext.vuong.tran@gxbank.net>
2024-10-30 17:56:58 +0700

    resilience apply timeout, fallback, retries pattern

--- order-service/src/main/java/com/example/orderservice/order/domain/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/order/domain/OrderService.java
@@ -3,8 +3,12 @@ package com.example.orderservice.order.domain;
 import com.example.orderservice.book.Book;
 import com.example.orderservice.book.BookClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Service
 public class OrderService {
@@ -25,7 +29,14 @@ public class OrderService {
         return bookClient.getBookByIsbn(isbn)
                 .map(book -> buildAcceptedOrder(book, quantity))
                 .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
-                .flatMap(orderRepository::save);
+                .flatMap(orderRepository::save)
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class, exception -> Mono.empty())
+                .retryWhen(
+                        Retry.backoff(3, Duration.ofMillis(100))
+                )
+                .onErrorResume(Exception.class, exception -> Mono.empty())
+                ; // define timeout + fallback
     }
 
     private static Order buildAcceptedOrder(Book book, int quantity) {
